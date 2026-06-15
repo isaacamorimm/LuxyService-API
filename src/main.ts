3750c92f4343b8f96c0aada1d825c70be4b8fad7
Isaac Amorim <163526904+isaacamorimm@users.noreply.github.com>
2026-06-15 14:37:12 -0300

    fix: add production domains to CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:8080', 'http://localhost:5174'],
+    origin: [
+      'http://localhost:8080',
+      'http://localhost:5174',
+      'https://luxyservice.com.br',
+      'https://www.luxyservice.com.br',
+    ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
